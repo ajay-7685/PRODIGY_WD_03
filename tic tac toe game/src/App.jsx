0071e9d02144bde0d2ee749928f9602a260bfd1d
ajay-7685 <157564015+ajay-7685@users.noreply.github.com>
2024-05-17 21:51:10 +0530

    Add files via upload

---
+++ tic tac toe game/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+import './App.css'
+import TicTacToe from './components/Tic-tac-toe/TicTacToe'
+
+function App() {
+  return(
+    <div className='main'>
+      <TicTacToe/>
+    </div>
+  )
+}
+
+export default App
